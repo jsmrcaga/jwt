fc71f50516a48a20287cfd52f7569329dbbc007e
Jo Colina <jo@jocolina.com>
2022-12-05 21:39:54 +0100

    Algorithms: add RSA algorithm

---
+++ src/errors.js
@@ -0,0 +1,5 @@
+class TokenError extends Error {}
+
+module.exports = {
+	TokenError
+};
---
+++ src/signature-algorithms.js
@@ -0,0 +1,37 @@
+const Crypto = require('crypto');
+const B64URL = require('./b64');
+const { TokenError } = require('./errors');
+// Each key should be a JOSE supported algorithm
+
+module.exports = {
+	sign: {
+		HS256: (token, secret_key) => {
+			const hmac = Crypto.createHmac('sha256', secret_key);
+			hmac.update(token);
+			return hmac.digest('base64');
+		},
+		RS256: (token, secret_key) => {
+			const sign = Crypto.createSign('RSA-SHA256');
+			sign.update(token);
+			return sign.sign(secret_key).toString('base64');
+		}
+	},
+	verify: {
+		HS256: (token_jose_and_payload, secret_key, signature) => {
+			const hmac = Crypto.createHmac('sha256', secret_key);
+			hmac.update(token_jose_and_payload);
+			const computed_signature = B64URL.toURLB64(hmac.digest('base64'));
+
+			if(computed_signature !== signature) {
+				throw new TokenError('Token: invalid signature');
+			}
+
+			return true;
+		},
+		RS256: (token_jose_and_payload, public_key, signature) => {
+			const verifier = Crypto.createVerify('RSA-SHA256');
+			verifier.update(token_jose_and_payload);
+			return verifier.verify(public_key, signature, 'base64');
+		}
+	}
+};
